d79bf4baf9a6f62de00aafa2a9299bcecb202847
Pawel Bak <PawelBak@staveleyhead.co.uk>
2020-05-14 16:22:21 +0100

    Prepare core App component, initialize redux store with root-reducer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import HomePage from "./pages/homepage/homepage.component";
+
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      App
-      <Switch>
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
