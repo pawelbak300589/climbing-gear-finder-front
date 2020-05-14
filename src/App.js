37e55a81aff63482710dbc95d5e0b811bb91aac7
Pawel Bak <PawelBak@staveleyhead.co.uk>
2020-05-14 16:25:20 +0100

    Revert "Prepare core App component, initialize redux store with root-reducer"
    This reverts commit d79bf4ba

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from "./pages/homepage/homepage.component";
-
 import './App.css';
 
 function App() {
     return (
         <div>
+            App
             <Switch>
-                <Route exact path="/" component={HomePage} />
             </Switch>
         </div>
     );
