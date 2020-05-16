7d576dfccc1cbb19526b4b880790b1ccdddfd770
Pawel Bak <pawelbak300589@gmail.com>
2020-05-16 18:02:14 +0100

    Add create brand form component

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -5,7 +5,7 @@ import './custom-button.styles.scss';
 
 const CustomButton = ({ children, type = "button", colour = "black", ...otherProps }) => {
     let content;
-    if (type === 'button') {
+    if (type === 'button' || type === 'submit') {
         content = <button className={`${colour} custom-button`} {...otherProps}>{children}</button>;
     } else if (type === 'link') {
         content = <Link className={`${colour} custom-button`} {...otherProps}>Create New Brand</Link>;
---
+++ src/components/forms/create-brand-form/create-brand-form.component.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CustomButton from "../../custom-button/custom-button.component";
+import FormInput from "../form-input/form-input.component";
+
+import { createBrand } from "../../../redux/brand/brand.actions";
+
+const CreateBrandForm = ({ createBrand }) => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const handleChange = (event) => {
+    //     const { value, name } = event.target;
+    //
+    //     // this.setState({ [name]: value });
+    }
+
+    return (
+        <div className="create-brand-form">
+            <h2>Create New Brand</h2>
+
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    name="name"
+                    type="text"
+                    label="Name"
+                    value=""
+                    handleChange={handleChange}
+                    required
+                />
+                <FormInput
+                    name="url"
+                    type="text"
+                    label="URL"
+                    value=""
+                    handleChange={handleChange}
+                    required
+                />
+                <FormInput
+                    name="img"
+                    type="text"
+                    label="Image"
+                    value=""
+                    handleChange={handleChange}
+                    required
+                />
+                <div className="buttons">
+                    <CustomButton type="submit" colour="green">Create</CustomButton>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+const mapDispatchToProps = dispatch => ({
+    createBrand: () => dispatch(createBrand())
+});
+
+export default connect(null, mapDispatchToProps)(CreateBrandForm);
