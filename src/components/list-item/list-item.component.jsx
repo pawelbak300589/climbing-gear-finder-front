436cfaa798b4c4c652af0cff874b1536889c124f
Pawel Bak <pawelbak300589@gmail.com>
2020-05-21 16:43:35 +0100

    Refactor list item component name

--- src/components/list-item/list-item.component.jsx
+++ src/components/list-item/list-item.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-import './list-item.scss';
+import './list-item.styles.scss';
 
 const ListItem = ({ item }) => {
     return (
